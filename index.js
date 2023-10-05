76c0c693db50fdd2bb11bb0e52a9b511808a6b3e
Rich Hildred <rhildred@gmail.com>
2023-10-05 13:44:50 -0400

    forgot to add

--- index.js
+++ index.js
@@ -4,41 +4,93 @@ import fs from 'fs';
 
 const app = express();
 
-app.get(/\.php$/, async (req, res) => {
-  const php = await NodePHP.load('8.2', {
-    emscriptenOptions: {
-      ENV: {
-        ...process.env,
-        TERM: 'xterm',
-      },
-    },
-  });
-  php.useHostFilesystem();
-  let sUrl = req.path;
-  if (sUrl[sUrl.length - 1] == '/') {
-    sUrl += 'index.php';
-  }
-  sUrl = sUrl.replace(/^\//, '');
-  if (!fs.existsSync(sUrl)) {
-    res.status(404).send(`cannot get ${sUrl}`);
-    return;
-  }
-  const args = [sUrl];
+const requestBodyToString = async (req) =>
+	await new Promise((resolve) => {
+		let body = '';
+		req.on('data', (chunk) => {
+			body += chunk.toString(); // convert Buffer to string
+		});
+		req.on('end', () => {
+			resolve(body);
+		});
+	});
 
-  const originalStdoutWrite = process.stdout.write.bind(process.stdout);
-  process.stdout.write = (chunk) => {
-    res.write(chunk);
-  };
-  try {
-    await php.cli(['php-cgi', ...args]);
-  } catch {
-    res.end();
-    return;
-  }
-  res.end();
-  process.stdout.write = originalStdoutWrite;
+
+
+const phpHandler = async (req, res) => {
+    const php = await NodePHP.load('8.2', {
+        emscriptenOptions: {
+            ENV: {
+                ...process.env,
+                TERM: 'xterm',
+            },
+        },
+    });
+    php.useHostFilesystem();
+    try {
+        const requestHeaders = {};
+        if (req.rawHeaders && req.rawHeaders.length) {
+            for (let i = 0; i < req.rawHeaders.length; i += 2) {
+                requestHeaders[req.rawHeaders[i].toLowerCase()] =
+                    req.rawHeaders[i + 1];
+            }
+        }
+
+        const body = requestHeaders['content-type']?.startsWith(
+            'multipart/form-data'
+        )
+            ? requestBodyToMultipartFormData(
+                req.body,
+                requestHeaders['content-type'].split('; boundary=')[1]
+            )
+            : await requestBodyToString(req);
+        let sUrl = req.path;
+        if (sUrl[sUrl.length - 1] == '/') {
+            sUrl += 'index.php';
+        }
+        sUrl = sUrl.replace(/^\//, '');
+        const data = {
+            url: req.url,
+            headers: requestHeaders,
+            method: req.method,
+            files: Object.fromEntries(
+                Object.entries((req).files || {}).map (
+                    ([key, file]) => [
+                        key,
+                        {
+                            key,
+                            name: file.name,
+                            size: file.size,
+                            type: file.mimetype,
+                            arrayBuffer: () => file.data.buffer,
+                        },
+                    ]
+                )
+            ),
+            body: body,
+            scriptPath: sUrl,
+        };
+        const resp = await php.run(data);
+        res.statusCode = resp.httpStatusCode;
+        Object.keys(resp.headers).forEach((key) => {
+            res.setHeader(key, resp.headers[key]);
+        });
+        res.end(resp.bytes);
+    } catch (e) {
+        console.log(e);
+    }
+
+}
+
+app.get(/\.php$/, async (req, res) => {
+    return phpHandler(req, res);
 });
 
+app.get("/", async (req, res) => {
+    return phpHandler(req, res);
+});
+
+
 const server = app.listen(3001, () =>
-  console.log(`listening on ${server.address().port}`)
+    console.log(`listening on ${server.address().port}`)
 );
